442e02719814473cd0d3c984e1f29a6c556df39e
pyhtin <pyhtin@1cps.ru>
2025-11-25 11:22:11 +0300

    исправление ошибок

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import { initPushButtonHandler } from "./push-notifications.js";
 import { updateUIOnLoad } from "./ui.js";
 import { checkSession } from "./api.js";
 import { initPreventDoubleTap } from "./utils.js";
+import { handleRegister, handleLogin } from "./authPasskey.js";
 
 // регистрируем sw
 if ('serviceWorker' in navigator) {
@@ -24,4 +25,19 @@ document.addEventListener('DOMContentLoaded', async () => {
   updateUIOnLoad();
   initPushButtonHandler();
   await checkSession();
+
+  // авторизация
+
+  const btnRegister = document.getElementById("register");
+  const btnLogin = document.getElementById("login");
+  const resultBlock = document.getElementById("result");
+
+
+  if (btnRegister) {
+    btnRegister.addEventListener('click', () => handleRegister(resultBlock, btnRegister));
+  }
+
+  if (btnLogin) {
+    btnLogin.addEventListener('click', () => handleLogin(resultBlock, btnLogin, btnRegister));
+  }
 });
